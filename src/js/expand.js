a29fdd320ebc3b58106106234d133ea692c0c403
Yash Gorasia <109784551+yashgo09@users.noreply.github.com>
2023-04-01 11:25:47 +0530

    Update FAQ expand script and style

--- src/js/expand.js
+++ src/js/expand.js
@@ -2,10 +2,9 @@ const faqWrappers = document.querySelectorAll(".faq-wrapper");
 
 faqWrappers.forEach((wrapper) => {
   const expandButton = wrapper.querySelector(".faq__expand");
-  const faqAnswer = wrapper.querySelector(".faq__answer");
 
   expandButton.addEventListener("click", () => {
-    faqAnswer.classList.toggle("expanded");
+    wrapper.classList.toggle("expanded");
     if (expandButton.innerText === "+") expandButton.innerText = "-";
     else expandButton.innerText = "+";
   });
